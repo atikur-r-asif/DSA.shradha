43ecdd034634df7d9bbd155582f83fdd94ad3f8c
ATIKUR RAHMAN ASIF <155958913+atikur-r-asif@users.noreply.github.com>
2025-11-03 21:54:21 +0600

    Add files via upload

---
+++ l2pp1.cpp
@@ -0,0 +1,20 @@
+//l2pp1  Sum of two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+     int a, b;
+     cout<<"---Sum of two numbers---"<<endl;
+
+     cout<<"Enter the first number: ";
+     cin>>a;
+     cout<<"Enter the second number: ";
+     cin>>b;
+
+     int sum=a+b;
+     cout<<"Sum of two numbers is: "<<sum<<endl;
+     cout<<"-------------";
+     
+     return 0;
+}
